03cec5af29146007e6f7b2043e9807c107e50e7d
pacheco4480 <david9pachecoo@gmail.com>
2024-07-22 15:53:20 +0100

    API products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {   
+        private readonly IProductRepository _productRepository;
+
+        //Construtor
+        //Ctrl  + . em cima do productRepository e clicar em "Create and assign field productRepository"
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        //Action que vai dar os produtos todos
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            //Vai buscar os produtos todos atraves do Repositorio _productRepository e o "Ok" mete tudo dentro do json
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
