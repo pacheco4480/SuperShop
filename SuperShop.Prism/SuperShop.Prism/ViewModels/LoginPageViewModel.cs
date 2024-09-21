d3991639c8cc62763ddcfc9b5a35e54c2c0df997
pacheco4480 <david9pachecoo@gmail.com>
2024-09-21 18:02:50 +0100

    Added Login and Products pages, along with API integration and image loading.

---
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperShop.Prism.ViewModels
+{
+    public class LoginPageViewModel : ViewModelBase
+    {   
+        //Atributos
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnabled;
+        private DelegateCommand _loginCommand;
+
+
+        //Construtor
+        public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Login";
+            IsEnabled = true;
+        }
+
+        //Comandos
+        //Se este atributo _loginCommand ja tem um objeto instanciado nao faz nada apenas executa-o mas se este
+        //atributo nao tiver um objeto instanciado instancia-o aqui "_loginCommand = new DelegateCommand()"
+        //Este objeto _loginCommand quando clicado vai executar um método que será o Login
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+
+
+        //Propriedades
+        //Quando quisermos só ler da view basta fazermos desta forma, quando quisermos
+        //ler e escrever na view temos que fazer como está em baixo em public string Password 
+        public string Email { get; set; }
+
+        public string Password 
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        //Métodos
+        private async void Login()
+        {
+
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an email.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an password.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            await App.Current.MainPage.DisplayAlert("Ok", "Boa, entrámos!!", "Accept");
+        }
+
+    }
+}
---
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/ProductsPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using SuperShop.Prism.Models;
+using SuperShop.Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperShop.Prism.ViewModels
+{
+    public class ProductsPageViewModel : ViewModelBase
+    {
+        //Atributos
+        private readonly IApiService _apiService;
+        // Atributo para Produtos
+        private List<ProductResponse> _products;
+
+        //Construtor
+        public ProductsPageViewModel(
+            INavigationService navigationService,
+            IApiService apiService) : base(navigationService)
+        {
+            _apiService = apiService;
+            Title = "Products Page";
+            LoadProductsAsync();
+        }
+
+        //Propriedade para Produtos
+        //Isto serve para pudermos usar a lista de Produtos (Temos entao que conjugar o Atributo + a Propriedade + o Método)
+        public List<ProductResponse> Products
+        {
+            get => _products;
+            set => SetProperty(ref _products, value);
+        }
+
+
+        //Método para criar os Produtos
+        private async void LoadProductsAsync()
+        {   //Esta URL está no ficheiro App.xaml
+            string url = App.Current.Resources["UrlAPI"].ToString();
+
+            //Ir buscar a API (aqui estamos a aceder ao ApiService.cs)
+            Response response = await _apiService.GetListAsync<ProductResponse>(url, "/api", "/Products");
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            Products = (List<ProductResponse>)response.Result;
+        }
+    }
+}
