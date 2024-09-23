384ecc54aa15843a93ab90d5ca5fddc98f2d785b
pacheco4480 <david9pachecoo@gmail.com>
2024-09-23 12:28:53 +0100

    Implemented search bar, Syncfusion BusyIndicator control, and navigation to ProductDetailPage

---
+++ SuperShop.Prism/SuperShop.Prism/ItemViewModels/ProductItemViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using SuperShop.Prism.Models;
+using SuperShop.Prism.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperShop.Prism.ItemViewModels
+{
+    //Tudo o que tem haver com dados nao devemos misturar mais nada nem qualquer tipo de interaçao isso é o que
+    //acontece no ProductResponse para implementarmos interaçao tivemos que criar esta classe ProductItemViewModel
+    //Em suma a classe ProductResponse so tem o produto enquanto que a classe ProductItemViewModel alem de ter os
+    //dados do produto vai ter a possiblidade de ser clicavel
+    public class ProductItemViewModel : ProductResponse
+    {
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectProductCommand;
+
+        public ProductItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public DelegateCommand SelectProductCommand =>
+            _selectProductCommand ??
+            (_selectProductCommand = new DelegateCommand(SelectProductAsync));
+
+        private async void SelectProductAsync()
+        {
+            NavigationParameters parameters = new NavigationParameters
+            {
+                {"product",this}
+            };
+            //Abre a página ProductDetailPage e passa os parametros product e o itemviewmodel
+            //Quando chamarmos esta pagina ProductDetailPage ele vai receber os parametros em ProductDetailPageViewModel em OnNavigatedTo
+            await _navigationService.NavigateAsync(nameof(ProductDetailPage), parameters);
+        }
+    }
+    }
--- SuperShop.Prism/SuperShop.Prism/ViewModels/ProductsPageViewModel.cs
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/ProductsPageViewModel.cs
@@ -1,26 +1,40 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using SuperShop.Prism.ItemViewModels;
 using SuperShop.Prism.Models;
 using SuperShop.Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace SuperShop.Prism.ViewModels
 {
     public class ProductsPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+
         //Atributos
         private readonly IApiService _apiService;
         // Atributo para Produtos
-        private List<ProductResponse> _products;
+        //No mobile nao se costuma usar LIst na vez disso usamos ObservableCollection
+        private ObservableCollection<ProductItemViewModel> _products;
+        //Roda dentada que aperece quando está a fazer loading da pagina
+        private bool _isRunning;
+        //Barra de pesquisa
+        private string _search;
+        private List<ProductResponse> _myProducts;
+        private DelegateCommand _searchCommand;
 
         //Construtor
         public ProductsPageViewModel(
             INavigationService navigationService,
             IApiService apiService) : base(navigationService)
         {
+            _navigationService = navigationService;
             _apiService = apiService;
             Title = "Products Page";
             LoadProductsAsync();
@@ -28,20 +42,54 @@ namespace SuperShop.Prism.ViewModels
 
         //Propriedade para Produtos
         //Isto serve para pudermos usar a lista de Produtos (Temos entao que conjugar o Atributo + a Propriedade + o Método)
-        public List<ProductResponse> Products
+        public ObservableCollection<ProductItemViewModel> Products
         {
             get => _products;
             set => SetProperty(ref _products, value);
         }
 
+        public DelegateCommand SearchCommand => _searchCommand ?? (_searchCommand = new DelegateCommand(ShowProducts));
+
+        public string Search
+        {
+            get => _search;
+            set
+            {
+                SetProperty(ref _search, value);
+                ShowProducts();
+            }
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+
 
         //Método para criar os Produtos
         private async void LoadProductsAsync()
-        {   //Esta URL está no ficheiro App.xaml
+        {   
+            //Se nao tiver ligação à internet
+            if (Connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {   //Mostra o Alert
+                    await App.Current.MainPage.DisplayAlert("Error", "Check internet connection", "Accept");
+                });
+                return; 
+            }
+
+            IsRunning = true;
+
+            //Esta URL está no ficheiro App.xaml
             string url = App.Current.Resources["UrlAPI"].ToString();
 
             //Ir buscar a API (aqui estamos a aceder ao ApiService.cs)
             Response response = await _apiService.GetListAsync<ProductResponse>(url, "/api", "/Products");
+
+            IsRunning = false;
 
             if (!response.IsSuccess)
             {
@@ -49,7 +97,50 @@ namespace SuperShop.Prism.ViewModels
                 return;
             }
 
-            Products = (List<ProductResponse>)response.Result;
+            _myProducts = (List<ProductResponse>)response.Result;
+            ShowProducts();
+        }
+
+        private void ShowProducts()
+        {
+            if (string.IsNullOrEmpty(Search))
+            {   //Aqui estamos a passar o valor dos produtos _myProducts e a inserir estes no ViewModel ProductItemViewModel
+                //ProductItemViewModel aqui ja temos o comando de seleção
+                Products = new ObservableCollection<ProductItemViewModel>(_myProducts.Select(p =>
+                new ProductItemViewModel(_navigationService)
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                    LastPurchase = p.LastPurchase,
+                    LastSale = p.LastSale,
+                    IsAvailable = p.IsAvailable,
+                    Stock = p.Stock,
+                    User = p.User,
+                    ImageFullPath = p.ImageFullPath
+                }).ToList());
+            }
+            else
+            {
+                Products = new ObservableCollection<ProductItemViewModel>(
+                    _myProducts.Select(p =>
+                    new ProductItemViewModel(_navigationService)
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl,
+                        LastPurchase = p.LastPurchase,
+                        LastSale = p.LastSale,
+                        IsAvailable = p.IsAvailable,
+                        Stock = p.Stock,
+                        User = p.User,
+                        ImageFullPath = p.ImageFullPath
+                    })
+                    .Where(p => p.Name.ToLower().Contains(Search.ToLower()))
+                    .ToList());
+            }
         }
     }
 }
